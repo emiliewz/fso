5c6ef9bf76ade15574a859779e052f2eaf2fe5e0
emilie <loveskyship@gmail.com>
2023-08-05 13:51:21 -0700

    The Phonebook Step4 v2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,23 @@ import { useState } from 'react'
 
 
 const App = () => {
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-1234567' }
+  // ])
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [kw, setKw] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const duplicate = persons.filter(p => p.name === newName).length !== 0
+    const duplicate = persons.filter(p => p.name.toLowerCase() === newName.toLowerCase()).length !== 0
     if (duplicate) {
       return alert(`${newName} is already added to phonebook`)
     }
@@ -24,7 +31,7 @@ const App = () => {
   }
 
   const filteredPersons = kw
-    ? persons.filter(p => p.name.includes(kw))
+    ? persons.filter(p => p.name.toLowerCase().includes(kw.toLowerCase()))
     : persons
 
   return (
