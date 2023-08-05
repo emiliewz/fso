82246f5a6cded21b2b7ea22a48bb5873d70850e1
emilie <loveskyship@gmail.com>
2023-08-05 11:58:36 -0700

    separate module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,16 +1,20 @@
 import Header from "./Header"
 import Content from "./Content"
 
+const Total = ({ total }) =>
+  <p><strong>total of {total} exercises</strong></p>
+
 const Course = ({ course }) => {
-  const total = course.parts.reduce((a, b) => {
-    return { exercises: a.exercises + b.exercises }
-  })
+  // const total = course.parts.reduce((a, b) => {
+  //   return { exercises: a.exercises + b.exercises }
+  // }).exercises
+  const total = course.parts.reduce((a, b) => a + b.exercises, 0)
 
   return (
     <>
       <Header course={course} />
       <Content parts={course.parts} />
-      <p><strong>total of {total.exercises} exercises</strong></p>
+      <Total total={total} />
     </>
   )
 }
