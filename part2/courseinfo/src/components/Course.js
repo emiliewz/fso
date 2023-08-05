931070abdffa22857059d223428a751eba542621
emilie <loveskyship@gmail.com>
2023-08-05 11:48:57 -0700

    Course information step8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,17 +2,15 @@ import Header from "./Header"
 import Content from "./Content"
 
 const Course = ({ course }) => {
-  // const total = course.parts.reduce((a, b) => {
-  //   return { exercises: a.exercises + b.exercises }
-  // })
-
-  const total = course.parts.reduce((a, b) => a + b.exercises, 0)
+  const total = course.parts.reduce((a, b) => {
+    return { exercises: a.exercises + b.exercises }
+  })
 
   return (
     <>
       <Header course={course} />
       <Content parts={course.parts} />
-      <p><strong>total of {total} exercises</strong></p>
+      <p><strong>total of {total.exercises} exercises</strong></p>
     </>
   )
 }
