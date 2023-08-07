90d29d6652955eee7d9a742d52306e5e6a448367
emilie <loveskyship@gmail.com>
2023-08-06 17:15:24 -0700

    Phonebook backend step8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,7 +5,9 @@ app.use(express.json())
 
 const morgan = require('morgan')
 
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
