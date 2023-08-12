3fa3f306e8066064c4054a6999f81fb3e694c781
emilie <loveskyship@gmail.com>
2023-08-12 15:50:32 -0700

    Phonebook database, step7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -13,6 +13,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -54,22 +56,16 @@ app.post('/api/persons', (request, response, next) => {
     })
   }
 
-  Person.find({ name: body.name })
-    .then(returnedPerson => {
-      if (returnedPerson) {
-        return response.redirect(`/api/persons/${returnedPerson.id}`)
-      }
-    })
-    .catch(error => next(error))
-
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -101,7 +97,12 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person
+    .findByIdAndUpdate(request.params.id, person, {
+      new: true,
+      runValidators: true,
+      context: 'query'
+    })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -4,8 +4,6 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connected to', url);
-
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
@@ -15,7 +13,11 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
   number: String,
 })
 
