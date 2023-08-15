3db9f6ab48ff27c8ff56dc6f5167e098dc03e5c9
emilie <loveskyship@gmail.com>
2023-08-14 17:35:46 -0700

    Blog list tests, step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('../utils/list_helper')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.blogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
   await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 }, 100000)
+
+test('the unique identifier property of the blog posts is named id', async () => {
+  const blogs = await api.get('/api/blogs')
+  expect(blogs.body[0].id).toBeDefined()
+})
+
+
 
 afterAll(async () => {
   await mongoose.connection.close()
