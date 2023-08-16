38e260d41d0b2e6225cc3a75b363d50db2709f2a
emilie <loveskyship@gmail.com>
2023-08-16 15:14:45 -0700

    bloglist expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-logger.info('connecting to', config.MONGODB_URI)
+// logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -22,5 +23,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
