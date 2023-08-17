10e325e8d2dee5f899c1b8cc814c34767994ae20
emilie <loveskyship@gmail.com>
2023-08-16 21:48:32 -0700

    bloglist expansion, step6

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -5,7 +5,6 @@ const User = require('../models/user')
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
-
 })
 
 blogsRouter.post('/', async (request, response) => {
