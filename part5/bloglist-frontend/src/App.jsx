6e01199e970680412c04c336ea9f6335be17e873
emilie <loveskyship@gmail.com>
2023-08-18 11:17:51 -0700

    bloglist frontend, step2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,27 +1,54 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    blogService
+      .getAll()
+      .then(blogs => setBlogs(blogs)
+      )
   }, [])
 
-  const handleLogin = (e) => {
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   const loginForm = () => (
-    <>
-      <h2>log in to application</h2>
-      <form>
+    <form onSubmit={handleLogin}>
+      <div>
         username
         <input
           type='text'
@@ -29,7 +56,8 @@ const App = () => {
           name='Username'
           onChange={({ target }) => setUsername(target.value)}
         />
-        <br />
+      </div>
+      <div>
         password
         <input
           type='password'
@@ -37,25 +65,33 @@ const App = () => {
           name='Password'
           onChange={({ target }) => setPassword(target.value)}
         />
-        <br />
-        <button type='button' onSubmit={handleLogin} >login</button>
-      </form>
-    </>
+      </div>
+      <button type='submit' >login</button>
+    </form>
   )
 
-  const blogList = () => (
-    <>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </>
-  )
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
 
   return (
     < div >
-      {!user && loginForm()}
-      {user && blogList()}
+      {errorMessage}
+      {!user && <div>
+        <h2>log in to application</h2>
+        {loginForm()}</div>}
+      {user && <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        <form onSubmit={handleLogout}>
+          <button type='submit'>logout</button>
+        </form>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>}
     </div >
   )
 }
