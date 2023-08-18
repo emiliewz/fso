ee09578535b15548c622d3d66329dda9bedc29fe
emilie <loveskyship@gmail.com>
2023-08-17 17:53:43 -0700

    blogs Router v2

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,59 +1,63 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const { blogsInDb } = require('../utils/list_helper')
+
 const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1 })
+    .find({})
+    .populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
-  const body = request.body
+  const { title, author, url, likes } = request.body
   const user = request.user
 
+  if (!user) {
+    return response.status(401).json({ error: 'operation not permitted' })
+  }
+
   const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
+    title, author, url,
+    likes: likes ? likes : 0,
     user: user._id
   })
 
   const savedBlog = await blog.save()
+
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
   response.status(201).json(savedBlog)
-
 })
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const id = request.params.id
   const user = request.user
 
-  const blog = await Blog.findById(id)
+  const blog = await Blog.findById(request.params.id)
 
-  if (blog.user.toString() === user.id.toString()) {
-    await Blog.findByIdAndRemove(id)
-    return response.status(204).end()
-  } else {
-    return response.status(400).json({ error: 'user invalid' })
+  if (!user || blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'operation not permitted' })
   }
+
+  user.blogs = user.blogs.filter(b => b.toString() !== blog.id.toString())
+
+  await user.save()
+  // await Blog.findByIdAndRemove(request.params.id)
+  await blog.deleteOne()
+
+  response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+  const { title, url, author, likes } = request.body
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, {
+    title, author, url, likes
+  }, { new: true })
   response.status(200).json(updatedBlog)
 })
 
