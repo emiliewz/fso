f9fa544a0ad4755aa4b3603926f2b75c567e941b
emilie <loveskyship@gmail.com>
2023-09-03 15:02:15 -0700

    etrieving anecdotes from the server using React Query

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,49 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './request'
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  } else if (result.isLoading) {
+    return <div>loading data</div>
+  }
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
