cd957a58e727fa04af7744c7e95bd7b21ad39602
emilie <loveskyship@gmail.com>
2023-09-13 15:12:57 -0700

    All authors

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,6 +98,13 @@ let books = [
 */
 
 const typeDefs = `
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -110,6 +117,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -117,7 +125,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: ({ name }) => {
+      const booksByAnthor = books.filter(b => b.author === name)
+      return booksByAnthor.length
+    }
   }
 }
 
