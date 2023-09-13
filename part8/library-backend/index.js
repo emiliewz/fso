48a5e3417e183e6fbfe1138831555e3fa284351c
emilie <loveskyship@gmail.com>
2023-09-13 15:28:47 -0700

    Books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,10 +126,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
+      if (!args.author && !args.genre) return books
 
-      const booksByAnthor = books.filter(b => b.author === args.author)
-      return booksByAnthor
+      if (args.author && args.genre) return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
+
+      if (args.author) return books.filter(b => b.author === args.author)
+
+      return books.filter(b => b.genres.includes(args.genre))
     },
     allAuthors: () => authors
   },
