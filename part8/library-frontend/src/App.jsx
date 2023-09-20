e19847a266d09a53e5b1bddda641f50ec3b392d4
emilie <loveskyship@gmail.com>
2023-09-20 10:51:16 -0700

    Adding a book

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { Link, Route, Routes } from 'react-router-dom'
+import { Alert } from 'react-bootstrap'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
   return (
     <div className='container'>
+      <Notify info={errorMessage} />
       <Link className='p-2 ps-0 text-decoration-none' to='/'>authors</Link>
       <Link className='p-2 text-decoration-none' to='/books'>books</Link>
       <Link className='p-2 text-decoration-none' to='/create'>add book</Link>
@@ -14,10 +25,20 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Authors />} />
         <Route path='/books' element={<Books />} />
-        <Route path='/create' element={<NewBook />} />
+        <Route path='/create' element={<NewBook setError={notify} />} />
       </Routes>
 
     </div>
+  )
+}
+
+const Notify = ({ info }) => {
+  if (!info) return null
+
+  return (
+    <Alert variant='danger'>
+      {info}
+    </Alert>
   )
 }
 
--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
+import { Button, Form, InputGroup } from 'react-bootstrap'
 
-const NewBook = () => {
+
+const NewBook = ({ setError }) => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [addBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors[0].message
+      setError(messages)
+    }
+  })
+
   const submit = async (event) => {
     event.preventDefault()
 
     console.log('add book...')
+    addBook({ variables: { title, author, published, genres } })
 
     setTitle('')
     setPublished('')
@@ -25,43 +38,53 @@ const NewBook = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={submit}>
-        <div>
-          title
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          published
-          <input
-            type="number"
-            value={published}
-            onChange={({ target }) => setPublished(target.value)}
-          />
-        </div>
-        <div>
-          <input
-            value={genre}
-            onChange={({ target }) => setGenre(target.value)}
-          />
-          <button onClick={addGenre} type="button">
-            add genre
-          </button>
-        </div>
-        <div>genres: {genres.join(' ')}</div>
-        <button type="submit">create book</button>
-      </form>
-    </div>
+    <Form className='my-3' onSubmit={submit}>
+
+      <Form.Group className='mb-3'>
+        <Form.Label>Title:</Form.Label>
+        <Form.Control
+          placeholder='title'
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3'>
+        <Form.Label>Author:</Form.Label>
+        <Form.Control
+          placeholder='author'
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3'>
+        <Form.Label>Published:</Form.Label>
+        <Form.Control
+          placeholder='published'
+          type='number'
+          value={published}
+          onChange={({ target }) => setPublished(parseInt(target.value))}
+        />
+      </Form.Group>
+
+      <InputGroup className='mb-3'>
+        <Form.Control
+          placeholder='genre'
+          value={genre}
+          onChange={({ target }) => setGenre(target.value)}
+        />
+        <Button variant='outline-info' onClick={addGenre}>
+          add genre
+        </Button>
+      </InputGroup>
+
+      <div className='mb-3'>
+        genres: {genres.join(' ')}
+      </div>
+
+      <Button variant='primary' type='submit'>create book</Button>
+    </Form >
   )
 }
 
