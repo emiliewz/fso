ceadc4b17f67ac25847702090c84a459a9d0c31a
emilie <loveskyship@gmail.com>
2023-10-04 16:15:55 -0700

    Command line p1

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -1,3 +1,21 @@
+interface BmiValues {
+  height: number,
+  weight: number
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight/(height/100)**2;
   if (bmi < 16.0) return 'Underweight (Severe thinness)';
@@ -5,9 +23,17 @@ const calculateBmi = (height: number, weight: number): string => {
   if (bmi <= 18.4) return 'Underweight (Mild thinness)';
   if (bmi <= 24.9) return 'Normal (healthy weight)';
   if (bmi <= 29.9) return 'Overweight (Pre-obese)';
-  if (bmi <= 34.9) return 'Obese (Class I)';
   if (bmi <= 39.9) return 'Obese (Class II)';
   return 'Obese (Class III)';
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const {height, weight} = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage);
+}
