737a36c4c55bf7079b49d3fe63cbe90040570ad1
emilie <loveskyship@gmail.com>
2023-10-04 17:43:17 -0700

    WebBMI

--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,11 +1,24 @@
 import express from 'express'
+import { calculateBmi } from './utils'
 const app = express()
 
-app.use('/hello', (_req, res) => {
+app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
 })
 
-const PORT = 3003
+app.get('/bmi', (req, res) =>{
+  if (!(req.query.height && !isNaN(Number(req.query.height))) || !(req.query.weight && !isNaN(Number(req.query.weight)))) {
+    res.json({error: 'malformatted parameters'})
+  }
+  const {height, weight} = req.query
+  res.json({
+    weight,
+    height,
+    bmi: calculateBmi(Number(height), Number(weight))
+  })
+})
+
+const PORT = 3002
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- part9/bmi/utils.ts
+++ part9/bmi/utils.ts
@@ -1,1 +1,12 @@
 export const isNotNumber = (argument: any): boolean => isNaN(Number(argument))
+
+export const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight/(height/100)**2;
+  if (bmi < 16.0) return 'Underweight (Severe thinness)';
+  if (bmi <= 16.9) return 'Underweight (Moderate thinness)';
+  if (bmi <= 18.4) return 'Underweight (Mild thinness)';
+  if (bmi <= 24.9) return 'Normal (healthy weight)';
+  if (bmi <= 29.9) return 'Overweight (Pre-obese)';
+  if (bmi <= 39.9) return 'Obese (Class II)';
+  return 'Obese (Class III)';
+}
