deaf7e9c076f3fd62a122706d1110f0f64192f30
emilie <loveskyship@gmail.com>
2023-10-08 16:35:29 -0700

    install cors

--- part9/flight-diaries/backend/src/index.ts
+++ part9/flight-diaries/backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import diaryRouter from './routes/diaries';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const PORT = 3000;
 
