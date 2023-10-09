b07c55da77cc76361b5feeaa1a2734a7c6be9e68
emilie <loveskyship@gmail.com>
2023-10-08 17:35:42 -0700

    Exercise 9.19

--- part9/flight-diaries/frontend/src/components/NewEntry.tsx
+++ part9/flight-diaries/frontend/src/components/NewEntry.tsx
@@ -32,31 +32,86 @@ const NewEntry = ({ diaries, setDiaries, setMsg }: {
   return (
     <div>
       <h2>Add new entry</h2>
+
       <form onSubmit={diaryCreation}>
         <div>
           date: <input 
+            type='date'
             value={date}
             onChange={({target}) => setDate(target.value)}
           />
         </div>
+
         <div>
-          visibility: <input 
-            value={visibility}
-            onChange={({target}) => setVisibility(target.value as Visibility)}
+          visibility: &nbsp; 
+          great<input
+            type='radio'
+            value='great'
+            name='visibility'
+            onChange={() => setVisibility('great' as Visibility)}
+          /> &nbsp; 
+          good<input
+            type='radio'
+            value='good'
+            name='visibility'
+            onChange={() => setVisibility('good' as Visibility)}
+          /> &nbsp; 
+          ok<input
+            type='radio'
+            value='ok'
+            name='visibility'
+            onChange={() => setVisibility('ok' as Visibility)}
+          /> &nbsp; 
+          poor<input
+            type='radio'
+            value='poor'
+            name='visibility'
+            onChange={() => setVisibility('poor' as Visibility)}
+          /> 
+        </div>
+
+        <div>
+          weather: &nbsp; 
+          sunny<input
+            type='radio'
+            value='sunny'
+            name='weather'
+            onChange={() => setWeather('sunny' as Weather)}
+          /> &nbsp; 
+          rainy<input
+            type='radio'
+            value='rainy'
+            name='weather'
+            onChange={() => setWeather('rainy' as Weather)}
+          /> &nbsp; 
+          cloudy<input
+            type='radio'
+            value='cloudy'
+            name='weather'
+            onChange={() => setWeather('cloudy' as Weather)}
+          /> &nbsp; 
+          stormy<input
+            type='radio'
+            value='stormy'
+            name='weather'
+            onChange={() => setWeather('stormy' as Weather)}
+          /> &nbsp; 
+          windy<input
+            type='radio'
+            value='windy'
+            name='weather'
+            onChange={() => setWeather('windy' as Weather)}
           />
         </div>
-        <div>
-          weather: <input 
-            value={weather}
-            onChange={({target}) => setWeather(target.value as Weather)}
-          />
-        </div>
+
         <div>
           comment: <input 
+            type='text'
             value={comment}
             onChange={({target}) => setComment(target.value)}
           />
         </div>
+        
         <button type='submit'>add</button>
       </form>
     </div>
