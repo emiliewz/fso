abd5c5dc0bbccb2a4adc591ab80c7b0d7e880d66
emilie <loveskyship@gmail.com>
2023-11-20 22:55:25 -0800

    update server using dist

--- part9/patientor/patientor-backend/src/index.ts
+++ part9/patientor/patientor-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import diagnosisRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
@@ -10,13 +11,13 @@ app.use(cors());
 
 const PORT = 3001;
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
-
 app.use('/api/diagnoses', diagnosisRouter);
 app.use('/api/patients', patientRouter);
+
+app.use(express.static(__dirname+'/../dist'));
+app.get('/*', (_req,res) => {
+  res.sendFile(path.join(__dirname+'/../dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
